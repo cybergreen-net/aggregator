1110da1396d69583aeb4f4794d52c1962f1be419
zelima <ir.mchedlishvili@gmail.com>
2016-10-07 13:48:19 +0400

    [load][s]: csv headers removed

--- load/load.sh
+++ load/load.sh
@@ -8,20 +8,20 @@ cg-stats-dev.crovisjepxcd.eu-west-1.rds.amazonaws.com \
 -U cybergreen \
 -d frontend \
 -p 5432 \
--c "\COPY count_by_risk FROM tmp/risk.csv WITH delimiter as ',' null '' csv header;"
+-c "\COPY count_by_risk FROM tmp/risk.csv WITH delimiter as ',' null '' csv;"
 
 psql -h \
 cg-stats-dev.crovisjepxcd.eu-west-1.rds.amazonaws.com \
 -U cybergreen \
 -d frontend \
 -p 5432 \
--c "\COPY count_by_country FROM tmp/country.csv WITH delimiter as ',' null '' csv header;"
+-c "\COPY count_by_country FROM tmp/country.csv WITH delimiter as ',' null '' csv;"
 
 psql -h \
 cg-stats-dev.crovisjepxcd.eu-west-1.rds.amazonaws.com \
 -U cybergreen \
 -d frontend \
 -p 5432 \
--c "\COPY count(risk, country, asn, date, period_type, count) FROM tmp/count.csv WITH delimiter as ',' null '' csv header;"
+-c "\COPY count(risk, country, asn, date, period_type, count) FROM tmp/count.csv WITH delimiter as ',' null '' csv;"
 
 rm -R tmp
