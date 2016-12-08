5e512cfb32e3f9b629179120667cd27c5c7b8f4e
zelima <ir.mchedlishvili@gmail.com>
2016-12-08 17:55:17 +0400

    [scripts][l][#27]: - restructured creation of table count - removed tables we don't need - aggregates on da level - amplification factor added

--- main.py
+++ main.py
@@ -1,13 +1,15 @@
 from __future__ import print_function
 
 from os.path import dirname, join
-
+from textwrap import dedent
+import psycopg2
+from psycopg2.extensions import AsIs
+import csv
 import os
 import tempfile
 import shutil
 import json
 import urllib
-import psycopg2
 import boto3
 
 def rpath(*args):
@@ -26,21 +28,9 @@ DEST_S3_KEY= join(STAGE,env['DEST_S3_KEY'])
 REFERENCE_KEY = join(STAGE, env['REFERENCE_KEY'])
 REDSHIFT_ROLE_ARN = env['REDSHIFT_ROLE_ARN']
 
-tablename = 'logentry'
 
-create_table_name = '''
-CREATE TABLE %s (
-   date timestamp,
-   ip   varchar(30),
-   risk int,
-   asn  bigint,
-   place varchar(3)
-   )
-''' % tablename
-
-print('Connecting ...')
 # Redshift connection
-connection = psycopg2.connect(
+connRedshift = psycopg2.connect(
     database=STAGE,
     user=env['REDSHIFT_USER'],
     password=env['REDSHIFT_PASSWORD'],
@@ -69,6 +59,7 @@ def create_manifest(datapackage,s3_bucket,s3_key):
             manifest['entries'].append({"url": join("s3://",s3_bucket,s3_key,key), "mandatory": True})
     return manifest
 
+
 def upload_manifest(tmp_dir):
     tmp_manifest = join(tmp_dir,'clean.manifest')
     s3bucket = SOURCE_S3_BUCKET
@@ -84,156 +75,128 @@ def upload_manifest(tmp_dir):
     key = join(SOURCE_S3_KEY, 'clean.manifest')
     obj = conns3.Object(s3bucket, key)
     obj.put(Body=open(tmp_manifest))
+    print('Manifest Updated')
 
+    
 ### LOAD, AGGREGATION, UNLOAD
-def create_table():
-    #CREATE REDSHIFT TABLE WHEN CSV FILE UPLOADED
-    cursor = connection.cursor();
-    cursor.execute("select exists(select * from information_schema.tables where table_name=%s)", (tablename,))
-    
-    if (cursor.fetchone()[0]):
-        cursor.execute("DROP TABLE %s" % (tablename))
-    
-    cursor.execute(create_table_name)
-    connection.commit();
+def create_redshift_tables():
+    tablenames = [
+        'dim_risk', 'logentry', 'count'
+    ]
+    cursor = connRedshift.cursor()
+    drop_tables(cursor, tablenames)
+    create_logentry = dedent('''
+    CREATE TABLE logentry(
+    date TIMESTAMP, ip VARCHAR(32), risk INT REFERENCES dim_risk(id),
+    asn BIGINT, country VARCHAR(2)
+    )
+    ''')
+    create_risk = dedent('''
+    CREATE TABLE dim_risk(
+    id INT PRIMARY KEY, slug VARCHAR(32), title VARCHAR(32),
+    amplification_factor FLOAT, description TEXT
+    )
+    ''')
+    create_count = dedent('''
+    CREATE TABLE count(
+    date TIMESTAMP, risk INT REFERENCES dim_risk(id), country VARCHAR(2),
+    asn BIGINT, count INT, count_amplified FLOAT
+    )
+    ''')
+    cursor.execute(create_risk)
+    cursor.execute(create_logentry)
+    cursor.execute(create_count)
+    connRedshift.commit()
+    print('Redshift tables created')
+
 
 def load_data():
-	role_arn = REDSHIFT_ROLE_ARN
-	manifest = join('s3://', SOURCE_S3_BUCKET, SOURCE_S3_KEY,'clean.manifest')
-	cursor = connection.cursor()
-	copycmd = '''
-COPY %s FROM '%s'
-CREDENTIALS 'aws_iam_role=%s'
-IGNOREHEADER AS 1
-DELIMITER ',' gzip
-TIMEFORMAT AS 'auto'
-MANIFEST;
-'''%(tablename, manifest, role_arn)
-	print('Loading data into db ... ')
-	cursor.execute(copycmd)
-	connection.commit()
-	print('Data Loaded')
+    role_arn = REDSHIFT_ROLE_ARN
+    manifest = join('s3://', SOURCE_S3_BUCKET, SOURCE_S3_KEY,'clean.manifest')
+    cursor = connRedshift.cursor()
+    copycmd = dedent('''
+    COPY logentry FROM '%s'
+    CREDENTIALS 'aws_iam_role=%s'
+    IGNOREHEADER AS 1
+    DELIMITER ',' gzip
+    TIMEFORMAT AS 'auto'
+    MANIFEST
+    ''')%(manifest, role_arn)
+    print('Loading data into db ... ')
+    cursor.execute(copycmd)
+    connRedshift.commit()
+    print('Data Loaded')
+
+
+def load_ref_data():
+    data = urllib.urlopen('https://raw.githubusercontent.com/cybergreen-net/refdata-risk/master/data/risk.csv')
+    risks = csv.DictReader(data)
+    cursor = connRedshift.cursor()
+    query = dedent("""
+    INSERT INTO dim_risk
+    VALUES (%(id)s, %(slug)s, %(title)s, %(amplification_factor)s, %(description)s)""")
+    for risk in risks:
+        # description is too long and not needed here
+        risk["description"]=""
+        cursor.execute(query,risk)
+    connRedshift.commit()
+
 
 def count_data():
-	cmd = 'SELECT count(*) FROM %s' % tablename
-	cursor = connection.cursor();
+	cmd = 'SELECT count(*) FROM logentry'
+	cursor = connRedshift.cursor()
 	cursor.execute(cmd)
 	print(cursor.fetchone()[0])
-	connection.commit()
+	connRedshift.commit()
 
-def create_count():
-    tablename = 'count'
-    copytable = 'logentry'
-    cursor = connection.cursor()
-    cursor.execute("select exists(select * from information_schema.tables where table_name=%s)", (tablename,))
-    if (cursor.fetchone()[0]):
-        cursor.execute("DROP TABLE %s"%(tablename))
-    create = """
-CREATE TABLE %s (
-risk int,
-country varchar(2),
-asn  bigint,
-date varchar(16),
-period_type varchar(8),
-count int
-)
-""" % (tablename)
-    cursor.execute(create)
-    connection.commit()
-    query = """
-INSERT INTO %s(risk, country, asn, date, period_type, count)
-(SELECT risk, place as country, asn, TO_CHAR(date, 'YYYY-MM-DD') as date, 'monthly', count(*) as count FROM 
-(SELECT DISTINCT (ip), date_trunc('week', date) AS date, risk, asn, place FROM %s) AS foo 
-GROUP BY TO_CHAR(date, 'YYYY-MM-DD'), asn, risk, place);
-"""%(tablename, copytable)
+
+def aggregate():
+    print('Aggregating ...')
+    cursor = connRedshift.cursor()
+    query = dedent("""
+    INSERT INTO count
+    (SELECT
+        date, risk, country, asn, count(*) as count, 0 as count_amplified
+    FROM(
+    SELECT DISTINCT (ip), date_trunc('day', date) AS date, risk, asn, country FROM logentry) AS foo
+    GROUP BY date, asn, risk, country ORDER BY date DESC, country ASC, asn ASC, risk ASC)
+    """)
     cursor.execute(query)
-    connection.commit()
-    print('%s table created'%(tablename))
+    connRedshift.commit()
 
-def create_count_by_country():
-	tablename = 'count_by_country'
-	copytable = 'count'
-	cursor = connection.cursor()
-	cursor.execute("select exists(select * from information_schema.tables where table_name=%s)", (tablename,))
-	if (cursor.fetchone()[0]):
-		  cursor.execute("DROP TABLE %s"%(tablename))
-	create = """
-CREATE TABLE %s (
-	risk int,
-	country varchar(2),
-	date varchar(16),
-	count bigint,
-	score real,
-	rank int
-)
-"""%(tablename)
-	cursor.execute(create)
-	connection.commit()
-	query = """
-INSERT INTO %s
-(SELECT risk, country, date, SUM(count) AS count, 0, 0
-FROM %s GROUP BY date, risk, country)
-"""%(tablename, copytable)
-	cursor.execute(query)
-	connection.commit()
-	print('%s table created'%(tablename))
+def update_amplified_count():
+    print('Calculating Aplificated Counts ...')
+    cursor = connRedshift.cursor()
+    query = dedent("""
+    UPDATE count
+    SET count_amplified = count.count*amplification_factor
+    FROM (SELECT * FROM count JOIN dim_risk on risk=id) AS foo
+    WHERE count.risk=foo.id
+    """)
+    cursor.execute(query)
+    connRedshift.commit()
+    print('Aggregation Finished!')
 
-def create_count_by_risk():
-	tablename = 'count_by_risk'
-	copytable = 'count_by_country'
-	cursor = connection.cursor()
-	cursor.execute("select exists(select * from information_schema.tables where table_name=%s)", (tablename,))
-	if (cursor.fetchone()[0]):
-			cursor.execute("DROP TABLE %s"%(tablename))
-	create = """
-CREATE TABLE %s (
-	risk int,
-	date varchar(16),
-	count bigint,
-	max bigint
-	)
-"""%(tablename)
-	cursor.execute(create)
-	connection.commit()
-	query = """
-INSERT INTO %s
-(SELECT risk, date, SUM(count), max(count)
-FROM %s GROUP BY date, risk)
-"""%(tablename, copytable)
-	cursor.execute(query)
-	connection.commit()
-	print('%s table created'%(tablename))
 
-def update_with_scores():
-	cursor = connection.cursor()
-	risktable = 'count_by_risk'
-	countrytable = 'count_by_country'
-	query = """
-UPDATE {0}
-SET score =
-CASE WHEN  LOG({1}.max) = 0 THEN 100
-ELSE 100 * (LOG({0}.count) / LOG({1}.max)) END
-FROM {1}
-WHERE {0}.risk = {1}.risk AND {0}.date = {1}.date;
-""".format(countrytable, risktable)
-	cursor.execute(query)
-	connection.commit()
-	
 def unload(table, s3path):
-    cursor = connection.cursor();
+    cursor = connRedshift.cursor()
     role_arn = REDSHIFT_ROLE_ARN
     s3bucket = join("s3://", DEST_S3_BUCKET)
-    aws_auth_args = 'aws_access_key_id=%s;aws_secret_access_key=%s'%(AWS_ACCESS_KEY, AWS_ACCESS_SECRET_KEY)
+    aws_auth_args = 'aws_access_key_id=%saws_secret_access_key=%s'%(AWS_ACCESS_KEY, AWS_ACCESS_SECRET_KEY)
     s3path = join(s3bucket, s3path)
-    cursor.execute("""
-UNLOAD('SELECT * FROM %s')
-TO '%s'
-CREDENTIALS '%s'
-DELIMITER AS ','
-ALLOWOVERWRITE
-PARALLEL OFF;
-"""%(table, s3path, aws_auth_args))
-    
+    cursor.execute(dedent("""
+    UNLOAD('SELECT * FROM count')
+    TO '%s'
+    CREDENTIALS '%s'
+    DELIMITER AS ','
+    ALLOWOVERWRITE
+    PARALLEL OFF
+    """)%(s3path, aws_auth_args))
+    add_extention('%s000'%(s3path))
+    delete_key('%s000'%(s3path))
+    print('Data Unloaded To s3')
+
+
 def add_extention(key):
     copy_source = {
         'Bucket': DEST_S3_BUCKET,
@@ -242,8 +205,10 @@ def add_extention(key):
     new_key = '%s.csv'%(key.split('0')[0])
     conns3.meta.client.copy(copy_source, DEST_S3_BUCKET, new_key)
 
+
 def delete_key(key):
     conns3.Object(DEST_S3_BUCKET, key).delete()
+
 
 ### LOAD FROM S3 TO RDS
 copy_commands = """
@@ -357,33 +322,28 @@ def create_indexes():
 		cursor.execute(idx_dict[idx])
 	connRDS.commit()
 
+def drop_tables(cursor, tables):
+    for tablename in tables:
+        cursor.execute("DROP TABLE IF EXISTS %(table)s CASCADE",{"table": AsIs(tablename)})
+
 
 if __name__ == '__main__':
     # AGGREGATION
     tmpdir = tempfile.mkdtemp()
     upload_manifest(tmpdir)
-    create_table()
-    load_data()
-    count_data()
-    create_count()
-    create_count_by_country()
-    create_count_by_risk()
-    update_with_scores()
-    # this needs to be automated 
-    table_keys = {
-        'count': join(DEST_S3_KEY,'count'),
-        'count_by_country': join(DEST_S3_KEY,'country'),
-        'count_by_risk': join(DEST_S3_KEY,'risk')
-    }
-    for table in table_keys:
-        unload(table, table_keys[table])    
-        add_extention('%s000'%(table_keys[table]))
-        delete_key('%s000'%(table_keys[table]))
-    print("Unloading datata to S3")
-    # LOAD TO RDS
-    print("Loading to RDS")
-    download(tmpdir)
-    create_tables()
-    os.system(copy_commands.format(tmp=tmpdir, password=env['RDS_PASSWORD'], host=env['RDS_HOST'], db=STAGE))
-    create_indexes()
     shutil.rmtree(tmpdir)
+    create_redshift_tables()
+    load_data()
+    load_ref_data()
+    count_data()
+    aggregate()
+    update_amplified_count()
+    unload_key = join(DEST_S3_KEY,'count')
+    unload('count', unload_key)
+    # print("Unloading datata to S3")
+    # # LOAD TO RDS
+    # print("Loading to RDS")
+    # download(tmpdir)
+    # create_tables()
+    # os.system(copy_commands.format(tmp=tmpdir, password=env['RDS_PASSWORD'], host=env['RDS_HOST'], db=STAGE))
+    # create_indexes()
